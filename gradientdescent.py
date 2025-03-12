1dc218c287b0dd969dff9d5e07631d05658080bb
Penquinz01 <janbaasjamal1@gmail.com>
2025-03-12 07:01:33 +0000

    fixed

--- gradientdescent.py
+++ gradientdescent.py
@@ -1,7 +1,8 @@
+import numpy as np
 def gradient_descent(a,b,c,d,x,learning_rate = 0.01,num_iterations = 10000):
     '''
-        J = (a*x**3 +b*x**2+c*x+d)**2
-        dJ_dx = 2*(a*x**3 + bx**2 + c*x +d)*(3a*x**2+2*b*x+c)
+        J = a*x**3 +b*x**2+c*x+d
+        dJ_dx = (3a*x**2+2*b*x+c)
 
         in each iterations
          x = x-learning_rate*dJ_dx
@@ -9,9 +10,12 @@ def gradient_descent(a,b,c,d,x,learning_rate = 0.01,num_iterations = 10000):
     '''
     for i in range(num_iterations):
         fx = loss(a,b,c,d,x)
+        if fx == 0:
+            break
         grad = derivatives(a,b,c,d,x,fx)
         x = x-learning_rate*grad
-
+        
+        
     return x
 def loss(a,b,c,d,x):
     return a*x**3 +b*x**2+c*x+d
@@ -19,13 +23,13 @@ def derivatives(a,b,c,d,x,fx):
     return 2*fx*(3*a*x**2+2*b*x+c)
 
 
-a =2
-b = -2
-c =2
-d =2
+a =float(input("Enter coefficient of x^3:"))
+b = float(input("Enter coefficient of x^2:"))
+c =float(input("Enter coefficient of x^1:"))
+d =float(input("Enter coefficient of x^0:"))
 
 #initial guess
-x = 0
+x = np.random.rand()
 x = gradient_descent(a,b,c,d,x)
 
 print(f"Approximate value of x is {x}")
